e2eb61b48b0a1285b8960d10e260211e71830392
Siarhei Hamanovich <hamanovich@gmail.com>
2024-10-20 18:28:54 +0200

    Close Auction SQS

--- auction-service/src/utils/closeAuction.mjs
+++ auction-service/src/utils/closeAuction.mjs
@@ -1,8 +1,8 @@
 import AWS from 'aws-sdk';
-// import logger from '../libs/logger.mjs';
+import logger from '../libs/logger.mjs';
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
-// const sqs = new AWS.SQS();
+const sqs = new AWS.SQS();
 
 export const closeAuction = async (auction) => {
   const params = {
@@ -17,52 +17,50 @@ export const closeAuction = async (auction) => {
     },
   };
 
-  const result = await dynamoDB.update(params).promise();
+  await dynamoDB.update(params).promise();
 
-  return result;
+  const {
+    title,
+    seller,
+    highestBid: { amount, bidder },
+  } = auction;
 
-  //   const {
-  //     title,
-  //     seller,
-  //     highestBid: { amount, bidder },
-  //   } = auction;
+  if (amount === 0) {
+    return await sqs
+      .sendMessage({
+        QueueUrl: process.env.MAIL_QUEUE_URL,
+        MessageBody: JSON.stringify({
+          subject: 'No bids on your auction item ;(',
+          recipient: seller,
+          body: `Oh no! Your item "${title}" didn't get any bids.`,
+        }),
+      })
+      .promise();
+  }
 
-  //   if (amount === 0) {
-  //     return await sqs
-  //       .sendMessage({
-  //         QueueUrl: process.env.MAIL_QUEUE_URL,
-  //         MessageBody: JSON.stringify({
-  //           subject: 'No bids on your auction item ;(',
-  //           recipient: seller,
-  //           body: `Oh no! Your item "${title}" didn't get any bids.`,
-  //         }),
-  //       })
-  //       .promise();
-  //   }
+  logger.info({ title, seller, amount, bidder });
 
-  //   logger.info({ title, seller, amount, bidder });
+  const notifySeller = sqs
+    .sendMessage({
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: 'Your item has been sold!',
+        recipient: seller,
+        body: `Woohoo! Your item "${title}" has been sold for $${amount}.`,
+      }),
+    })
+    .promise();
 
-  //   const notifySeller = sqs
-  //     .sendMessage({
-  //       QueueUrl: process.env.MAIL_QUEUE_URL,
-  //       MessageBody: JSON.stringify({
-  //         subject: 'Your item has been sold!',
-  //         recipient: seller,
-  //         body: `Woohoo! Your item "${title}" has been sold for $${amount}.`,
-  //       }),
-  //     })
-  //     .promise();
+  const notifyBidder = sqs
+    .sendMessage({
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: 'You won an auction!',
+        recipient: bidder,
+        body: `What a great deal! You got yourself a "${title}" for $${amount}`,
+      }),
+    })
+    .promise();
 
-  //   const notifyBidder = sqs
-  //     .sendMessage({
-  //       QueueUrl: process.env.MAIL_QUEUE_URL,
-  //       MessageBody: JSON.stringify({
-  //         subject: 'You won an auction!',
-  //         recipient: bidder,
-  //         body: `What a great deal! You got yourself a "${title}" for $${amount}`,
-  //       }),
-  //     })
-  //     .promise();
-
-  //   return Promise.all([notifySeller, notifyBidder]);
+  return Promise.all([notifySeller, notifyBidder]);
 };
